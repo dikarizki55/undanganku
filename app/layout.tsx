4c2e7850081f54e8b59674bcedb66865ae5e837f
dikarizki55 <dikaesmidorikawa55@gmail.com>
2025-04-08 12:31:00 +0700

    tambah metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <Head>
+        <meta property="og:description" content="Mohon datang yaaa" />
+        <meta property="og:image" content="/images/page1/bg1.jpg" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
